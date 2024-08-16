ff628705116974528eafa7ca42dfdabab8b160e8
Nguyễn Thùy Dung <nthuydung.1603@gmail.com>
2024-08-16 19:35:06 +0700

    lesson1

---
+++ lesson1.js
@@ -0,0 +1,79 @@
+// console.log("Hello Word!");
+
+// {
+//   let a = 3;
+//   {
+//     console.log(a);
+//   }
+// }
+// console.log(a);
+
+// let a = null;
+// console.log(a);
+
+// let a;
+
+// {
+//   var a = 7;
+// }
+// console.log(a);
+
+// var a = 7;
+// var a = 11;
+// console.log(a);
+
+// const PI = 3.14;
+
+// PI = 3;
+
+// const objA = {};
+// console.log(objA);
+// objA["name"] = "Mazie";
+// console.log(objA);
+
+// {
+//   x = 3;
+// }
+// console.log();
+
+// let a = 3;
+// let b = 4;
+// let c = a + b; //a**b = 2^4
+// console.log(c);
+
+// let a = 10;
+// let b = 10 % 3;
+// console.log(b);
+
+//a++ in a trước -> + a
+// let a = 1;
+// console.log(++a);
+// console.log(a);
+
+// let a = 1;
+// console.log(a--);
+// console.log(a);
+
+// let a = 2;
+// let b = 2;
+// a **= b;
+// console.log(a);
+
+// let a = 3;
+// let b = 45;
+// console.log(a >= b);
+// console.log(a === b);
+
+// let a = "Mazie";
+// let b = "so cute";
+// console.log(`${a} so ${b}`);
+
+// console.log(5 + "4");
+// console.log("5" === 5);
+
+// let a = 5;
+// console.log(a % 2 == 0 && a > 0);
+
+// console.log(a % 2 == 0 || a < 6 || a > 0);
+
+// console.log(a % 2 == 0 && (a > 6 || a > 0));
---
+++ lesson2.js
@@ -0,0 +1,10 @@
+// const age = 23;
+// const drink = age >= 18 ? "wine" : "water";
+// console.log(drink);
+
+// a = undefined;
+// b = a ?? 1111; //nếu giá trị trước null || undefined thì gắn giá trị sau
+// console.log(b);
+
+// let a = 100;
+// console.log(typeof a);
